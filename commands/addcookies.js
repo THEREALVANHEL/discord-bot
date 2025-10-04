68d951c8ab7582de547e30b061062dc0df5c3568
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-04 10:26:19 +0530

    Create addcookies.js

---
+++ commands/addcookies.js
@@ -0,0 +1,58 @@
+// commands/addcookies.js
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const User = require('../models/User');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('addcookies')
+    .setDescription('Add cookies to a user.')
+    .addUserOption(option =>
+      option.setName('target')
+        .setDescription('The user to add cookies to')
+        .setRequired(true))
+    .addIntegerOption(option =>
+      option.setName('amount')
+        .setDescription('The amount of cookies to add')
+        .setRequired(true)),
+  async execute(interaction) {
+    const targetUser = interaction.options.getUser('target');
+    const amount = interaction.options.getInteger('amount');
+
+    if (amount <= 0) {
+      return interaction.reply({ content: 'Amount must be a positive number.', ephemeral: true });
+    }
+
+    let user = await User.findOne({ userId: targetUser.id });
+    if (!user) {
+      user = new User({ userId: targetUser.id });
+    }
+
+    user.cookies += amount;
+    await user.save();
+
+    // Update cookie roles
+    const member = interaction.guild.members.cache.get(targetUser.id);
+    if (member) {
+      const cookieRoles = interaction.client.config.cookieRoles;
+      for (const roleConfig of cookieRoles) {
+        if (member.roles.cache.has(roleConfig.roleId)) {
+          await member.roles.remove(roleConfig.roleId).catch(() => {});
+        }
+      }
+      const newCookieRole = cookieRoles
+        .filter(r => r.cookies <= user.cookies)
+        .sort((a, b) => b.cookies - a.cookies)[0];
+      if (newCookieRole) {
+        await member.roles.add(newCookieRole.roleId).catch(() => {});
+      }
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle('Cookies Added')
+      .setDescription(`Added ${amount} cookies to ${targetUser.tag}. They now have ${user.cookies} cookies.`)
+      .setColor(0x00FF00)
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [embed] });
+  },
+};
