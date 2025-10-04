7f7ec15ee538a397998f6767e74481852e6603cd
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-04 10:09:21 +0530

    Create guildMemberRemove.js

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: 'guildMemberRemove',
+  async execute(member, client) {
+    try {
+      const settings = await require('../models/Settings').findOne({ guildId: member.guild.id });
+      if (settings && settings.leaveChannelId) {
+        const channel = member.guild.channels.cache.get(settings.leaveChannelId);
+        if (channel) {
+          channel.send({
+            content: `${member.user.tag} left the server on <t:${Math.floor(Date.now() / 1000)}:F>`,
+            files: ['https://tenor.com/view/flcl-mamimi-aaahhh-wtf-shocked-gif-24981847.gif'],
+          });
+        }
+      }
+    } catch (error) {
+      console.error('Error in guildMemberRemove event:', error);
+    }
+  },
+};
