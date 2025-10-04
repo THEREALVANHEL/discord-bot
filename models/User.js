bebee4a1a491451fc642a2d1472768ac287ac251
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-04 10:06:06 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  xp: { type: Number, default: 0 },
+  level: { type: Number, default: 0 },
+  cookies: { type: Number, default: 0 },
+  coins: { type: Number, default: 0 },
+  lastDaily: { type: Date, default: null },
+  // Add more fields as needed
+});
+
+module.exports = mongoose.model('User ', userSchema);
