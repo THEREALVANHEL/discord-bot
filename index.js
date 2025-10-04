d3b0afe72a2e8848c23d5ce8dedc44807ed1ce1b
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-04 10:05:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,24 +16,49 @@ const client = new Client({
 });
 
 client.commands = new Collection();
+client.cooldowns = new Collection();
+client.config = {
+  guildId: process.env.GUILD_ID,
+  roles: {
+    autoJoin: '1384141744303636610',
+    leadMod: '1371147562257748050',
+    mod: '1371728518467293236',
+    cookiesManager: '1372121024841125888',
+    forgottenOne: '1376574861333495910',
+    overseer: '1371004219875917875',
+    gamelog:User  '1371003310223654974',
+  },
+  levelingRoles: [
+    { level: 30, roleId: '1371032270361853962' },
+    { level: 60, roleId: '1371032537740214302' },
+    { level: 120, roleId: '1371032664026382427' },
+    { level: 210, roleId: '1371032830217289748' },
+    { level: 300, roleId: '1371032964938600521' },
+    { level: 450, roleId: '1371033073038266429' },
+  ],
+  cookieRoles: [
+    { cookies: 5000, roleId: '1371004762761461770' },
+    { cookies: 3000, roleId: '1371001806930579518' },
+    { cookies: 1750, roleId: '1371001322131947591' },
+    { cookies: 1000, roleId: '1371000389444305017' },
+    { cookies: 500, roleId: '1370999721593671760' },
+    { cookies: 100, roleId: '1370998669884788788' },
+  ],
+};
 
-// Load commands dynamically
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
+  const command = require(path.join(commandsPath, file));
   if (command.data && command.execute) {
     client.commands.set(command.data.name, command);
   }
 }
 
-// Load events dynamically
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
+  const event = require(path.join(eventsPath, file));
   if (event.once) {
     client.once(event.name, (...args) => event.execute(...args, client));
   } else {
@@ -41,7 +66,6 @@ for (const file of eventFiles) {
   }
 }
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -49,5 +73,4 @@ mongoose.connect(process.env.MONGODB_URI, {
   console.log('Connected to MongoDB');
 }).catch(console.error);
 
-// Login to Discord
 client.login(process.env.DISCORD_TOKEN);
