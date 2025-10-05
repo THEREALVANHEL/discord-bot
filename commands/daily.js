e5473a25f2d2b317acef7394dfa48f8caccbe7f6
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-05 18:45:06 +0530

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,4 +1,4 @@
-// commands/daily.js (REPLACE - Added 7-Day Streak Bonus Logic + Fixed Syntax)
+// commands/daily.js (REPLACE - Fixed infinite role add/remove loop)
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const User = require('../models/User');
 const Settings = require('../models/Settings');
@@ -76,16 +76,29 @@ module.exports = {
       const member = interaction.guild.members.cache.get(interaction.user.id);
       if (member) {
         const levelingRoles = interaction.client.config.levelingRoles;
+        
+        // FIX: Find the single highest eligible role
+        const targetLevelRole = levelingRoles
+            .filter(r => r.level <= user.level)
+            .sort((a, b) => b.level - a.level)[0];
+        
+        const targetLevelRoleId = targetLevelRole ? targetLevelRole.roleId : null;
+
         for (const roleConfig of levelingRoles) {
-          if (member.roles.cache.has(roleConfig.roleId)) {
-            await member.roles.remove(roleConfig.roleId).catch(() => {});
+          const roleId = roleConfig.roleId;
+          const hasRole = member.roles.cache.has(roleId);
+          
+          if (roleId === targetLevelRoleId) {
+              // If this is the correct role but the user doesn't have it, add it.
+              if (!hasRole) {
+                  await member.roles.add(roleId).catch(() => {});
+              }
+          } else {
+              // If the user has a different leveling role, remove it.
+              if (hasRole) {
+                  await member.roles.remove(roleId).catch(() => {});
+              }
           }
-        }
-        const newLevelRole = levelingRoles
-          .filter(r => r.level <= user.level)
-          .sort((a, b) => b.level - a.level)[0];
-        if (newLevelRole) {
-          await member.roles.add(newLevelRole.roleId).catch(() => {});
         }
       }
       
