900dc8d4b28c88846ca8dde07271743f638f7ea9
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-29 19:10:23 +0530

    Update delay.js

--- utils/delay.js
+++ utils/delay.js
@@ -1,4 +1,7 @@
-// utils/delay.js (Placeholder)
-export function delay(ms) {
+// utils/delay.js (REPLACED - Converted to CommonJS)
+
+function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+module.exports = { delay };
