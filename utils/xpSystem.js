399899eb09f9807e891c15ce22e750513e8b594c
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-29 14:54:39 +0530

    Create xpSystem.js

---
+++ utils/xpSystem.js
@@ -0,0 +1,3 @@
+// utils/xpSystem.js (Placeholder)
+export const XP_COOLDOWN = 60000; // 1 minute cooldown (60000ms)
+export const generateXP = () => Math.floor(Math.random() * 15) + 10; // 10-25 XP
