26ede20d5a74fd94e04bff77b7f9154516920fa4
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-29 14:56:52 +0530

    Create truncateText.js

---
+++ utils/truncateText.js
@@ -0,0 +1,8 @@
+// utils/truncateText.js (Placeholder)
+export function truncateText(text, maxLength) {
+    if (text.length <= maxLength) {
+        return text;
+    }
+    return text.substring(0, maxLength - 3) + '...';
+}
+```<ctrl63>
