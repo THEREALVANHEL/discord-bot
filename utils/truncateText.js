33facc956d8634187b74705622a89a3aae99d085
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-29 15:03:49 +0530

    Update truncateText.js

--- utils/truncateText.js
+++ utils/truncateText.js
@@ -1,8 +1,8 @@
-// utils/truncateText.js (Placeholder)
+// utils/truncateText.js
 export function truncateText(text, maxLength) {
     if (text.length <= maxLength) {
         return text;
     }
+    // Subtract 3 for the '...'
     return text.substring(0, maxLength - 3) + '...';
 }
-```<ctrl63>
