3462d629cbf387a698a78fe46d1bc55d1190fd1f
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-29 19:11:21 +0530

    Update processCommand.js

--- utils/processCommand.js
+++ utils/processCommand.js
@@ -1,7 +1,13 @@
-// utils/processCommand.js (Placeholder)
+// utils/processCommand.js (REPLACED - Converted to CommonJS)
 // Handles execution of AI-suggested commands that are not hardcoded in messageCreate.js
-export async function processCommand(client, message, name, args) {
+
+async function processCommand(client, message, name, args) {
     console.log(`[UTILITY] Mocking command process: /${name} ${args.join(' ')}`);
     // Return true to indicate the command was 'handled'
-    return true; 
+    // In a real implementation, you might try to find and execute the command
+    // const command = client.commands.get(name);
+    // if (command) { /* try to execute? Need to adapt interaction model */ }
+    return true;
 }
+
+module.exports = { processCommand };
