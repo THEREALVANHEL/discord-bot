47601608371a10037b90e1819dfaf775b28eed90
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-29 14:54:55 +0530

    Create createNewUser.js

---
+++ utils/createNewUser.js
@@ -0,0 +1,5 @@
+// utils/createNewUser.js (Placeholder)
+// This utility is not used in the provided messageCreate.js, but is included for completeness.
+export function createNewUser(userId) {
+    return { userId, xp: 0, level: 0, save: async () => {} };
+}
