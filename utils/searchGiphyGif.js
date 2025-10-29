7bc7da328a35831824e94634fb06f9540bd5fcd9
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-29 14:56:02 +0530

    Create searchGiphyGif.js

---
+++ utils/searchGiphyGif.js
@@ -0,0 +1,6 @@
+// utils/searchGiphyGif.js (Placeholder)
+// Searches Giphy for a GIF URL.
+export async function searchGiphyGif(term) {
+    // Return a default GIF URL
+    return 'https://media.giphy.com/media/l4pTsh45Dg7ClzJny/giphy.gif';
+}
