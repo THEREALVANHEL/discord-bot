94c2fe3b762fc29a494d49df2652cbd382de1d5a
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-29 19:12:37 +0530

    Update xpSystem.js

--- utils/xpSystem.js
+++ utils/xpSystem.js
@@ -1,3 +1,9 @@
-// utils/xpSystem.js (Placeholder)
-export const XP_COOLDOWN = 60000; // 1 minute cooldown (60000ms)
-export const generateXP = () => Math.floor(Math.random() * 15) + 10; // 10-25 XP
+// utils/xpSystem.js (REPLACED - Converted to CommonJS)
+
+const XP_COOLDOWN = 60000; // 1 minute cooldown (60000ms)
+const generateXP = () => Math.floor(Math.random() * (25 - 10 + 1)) + 10; // 10-25 XP
+
+module.exports = {
+    XP_COOLDOWN,
+    generateXP,
+};
