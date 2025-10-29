fbed358a9bd8ad9f7f71d0f2e52532df6de0c5e6
THEREALVANHEL <vishalkumarreddy01@gmail.com>
2025-10-29 19:10:12 +0530

    Update createNewUser.js

--- utils/createNewUser.js
+++ utils/createNewUser.js
@@ -1,5 +1,9 @@
-// utils/createNewUser.js (Placeholder)
+// utils/createNewUser.js (REPLACED - Converted to CommonJS)
 // This utility is not used in the provided messageCreate.js, but is included for completeness.
-export function createNewUser(userId) {
+
+// Assuming placeholder logic
+function createNewUser(userId) {
     return { userId, xp: 0, level: 0, save: async () => {} };
 }
+
+module.exports = { createNewUser };
